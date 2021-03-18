d437cf6a771d58fbf8c1022489c6e6cb26fdb0d5
Seung <osy044@naver.com>
2021-03-18 17:25:09 +0900

    commit changes

--- src/pages/mainpage/MainPage.js
+++ src/pages/mainpage/MainPage.js
@@ -6,15 +6,19 @@ function MainPage() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Seung's hompage</p>
-        <a
-          className="App-link"
-          href="https://oneclickai.co.kr/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          OneClick Ai
-        </a>
+        <p>
+          Hello, i'm Seung. <br></br>
+          I'm interested in technologies, teaching and arts! <br></br>
+          I'd like to make small but better changes to the world.
+        </p>
+        <p>Please contact me through my email, github.</p>
+        <a href="mailto:osy044@naver.com">Email</a>
+        <a href="https://github.com/SeungOhh">Github</a>
+
+        <p>Works that I did or contributed</p>
+        <a href="https://oneclickai.co.kr/">OneClickAI</a>
+        <a href="/">Vehicle modeling and control</a>
+        <a href="/">Aerodynamics around flapping wings</a>
       </header>
     </div>
   );
